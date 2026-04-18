aa247312de55de1642dfefd0f8977e267547c601
okochi-mika <hiyoko_m@icloud.com>
2026-04-18 23:34:40 +0900

    修正：mikuni_392dayフォルダの中身が空になる問題を解決

---
+++ mikuni_392day/setlist.js
@@ -0,0 +1,12 @@
+// setlist.js の冒頭
+const setlistData = [
+    { title: "1. Naked ", id: "06PuQc6pwII" }, // YouTube IDを調べて入れる
+    { title: "2. 遠い星", id: "0Cz_jmdLYrE" },
+    { title: "3. all the way", id: "ZjvQ6Vj_wuw" }, 
+    { title: "4. If ~もしも願いが叶うなら~", id: "ef1JsUS7Qkk" },
+    { title: "5. 夏花火", id: "KKYMYE1gyNI" },
+    { title: "6. はじまり", id: "8TB_eiJH47U" },
+    { title: "7. それが、愛でしょう", id: "TMoOdbjcjGU" },
+    { title: "8. Alone", id: "JjWwfUbOP0w" },
+    { title: "9. この場所が好きで", id: "tMR3hb4iR34" }
+];
