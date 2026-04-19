976abd637db50ff11675644aca4501b83ac2a8bb
okochi-mika <hiyoko_m@icloud.com>
2026-04-19 11:04:33 +0900

    リクエストアワード追加

---
+++ mikuni_request_award/setlist.js
@@ -0,0 +1,22 @@
+// setlist.js の冒頭
+const setlistData = [
+    { title: "1. 君がいるから ", id: "npE8RBnBsAk" }, // YouTube IDを調べて入れる
+    { title: "2. 君に吹く風", id: "iPkuvrQaJQg" },
+    { title: "3. 2000EXPRESS", id: "EHD0Tm-wji0" }, 
+    { title: "4. TRUE", id: "oIXAMbXmlVw" },
+    { title: "5. 枯れない花", id: "HKOy3b5ZSVQ" },
+    { title: "6. この場所が好きで", id: "tMR3hb4iR34" },
+    { title: "7. Alone", id: "JjWwfUbOP0w" },
+    { title: "8. 初恋の君", id: "vBfJBE0KqPc" },
+    { title: "9. キミノユメ", id: "HEHvGgZsdl8" },
+    { title: "10. それが、愛でしょう", id: "TMoOdbjcjGU" },
+    { title: "11. 39", id: "JyDdk_IBc_A" },
+    { title: "12. サヨウナラ! 20世紀の僕たち", id: "9u8OTofjYuI" },
+    { title: "13. さよならも言えなかった夏", id: "dgS2dJ_VE78" },
+    { title: "14. KOHAKU", id: "FZsIi1KH4xI" },
+    { title: "15. Life", id: "0mTnSzDFhyE" },
+    { title: "16. 南風", id: "f8Nv-5wIlbg" },
+    { title: "17. Naked", id: "06PuQc6pwII" },
+    { title: "18. イ〜じゃナイ!?", id: "2tIb-6kiNZI" },
+    { title: "19. Remember", id: "iDacFs674Kc" }
+];
